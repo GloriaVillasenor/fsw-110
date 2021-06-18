e47bfd8436e8c78f40796332bd600c4cf9c2e7e6
GLORIA VILLASENOR <82358971+GloriaVillasenor@users.noreply.github.com>
2021-06-18 09:07:01 -0700

    Added project1 to week2

--- week2/project1.js
+++ week2/project1.js
@@ -1,4 +1,27 @@
 var h1Element = document.createElement('h1');
 h1Element.textContent = 'Welcome to my JS site';
-var para = document.querySelector('All of this was created with Javascript');
-console.log(para.textContent);
+document.body.appendChild(h1Element)
+
+var para = document.createElement('p');
+para.textContent = 'All of this was created with Javascript';
+console.log(para);
+document.body.appendChild(para)
+
+var newList = document.createElement('ol');
+newList.setAttribute('id', 'startingList')
+document.body.appendChild(newList)
+
+var newListItem = document.createElement('li')
+newListItem.textContent = 'item one'
+document.getElementById('startingList').appendChild(newListItem)
+
+var newListItemTwo = document.createElement('li')
+newListItemTwo.textContent = 'item two'
+document.getElementById('startingList').appendChild(newListItemTwo)
+
+var newListItemThree = document.createElement('li')
+newListItemThree.textContent = 'item three'
+document.getElementById('startingList').appendChild(newListItemThree)
+
+var myList = document.gegtElementById('my-list')
+myList.appendChild(newListItem)
---
+++ week2/project2.js
@@ -0,0 +1,5 @@
+var header = document.createElement('h1')
+header.textContent = 'Home Content Contact'
+header.setAttribute('id', 'navagation')
+document.body.appendChild(header)
+document.getElementById('navagation').style.textAlign = 'center'
