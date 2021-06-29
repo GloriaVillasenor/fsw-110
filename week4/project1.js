df902e23e03c1321837b375309740833d4d1a7d8
GLORIA VILLASENOR <82358971+GloriaVillasenor@users.noreply.github.com>
2021-06-29 12:45:48 -0700

    added project1 to week5

--- week4/project1.js
+++ week4/project1.js
@@ -1,3 +1,36 @@
-const button = document.getElementById('button')
-button.addEventListener('onmouseover', buttonChange)
+const button = document.getElementById('box')
+button.addEventListener('mouseover', buttonChange)
 
+button.addEventListener('unload', completeLoading)
+
+function completeLoading() {
+    button,style.backgroundColor = "black"
+}
+
+function buttonChange() {
+    button.style.backgroundColor = "green"
+}
+
+button.addEventListener('mousedown', holdButton)
+
+function holdButton() {
+    button.style.backgroundColor = "yellow"
+}
+
+button.addEventListener('mousedown', goOver)
+
+function goOver() {
+    buttton.style.backgroundColor = "blue"
+}
+
+button.addEventListener("dblclick", changeTwo)
+
+function changeTwo() {
+    button.style.backgroundColor = "red"
+}
+
+button.addEventListener('scroll', movePage)
+
+function movePage() {
+    button.style.backgroundColor = "purple"
+}
