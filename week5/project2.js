96271f71c558a10282db2d5f853dcefe256c04ec
GLORIA VILLASENOR <82358971+GloriaVillasenor@users.noreply.github.com>
2021-07-06 15:05:56 -0700

    Added project2 to week5

--- week5/project2.js
+++ week5/project2.js
@@ -1,1 +1,20 @@
-const inputElement = document.getElementById('')
+const form = document.customerData
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+})
+function alertMess() {
+var firstPart = form.first_name.value
+var secondPart = form.last_name.value
+var thirdPart = form.age.value
+var fourthPart = form.decision.value
+var sixth = form.satisfied.value
+
+var checkedInput = []
+for(let i = 0; i < form.renewl.length; i++) {
+    if (form.renewl[i].checked){
+        checkedInput.push(form.renewl[i].value)
+    }
+}
+var allPart = firstPart + " " + secondPart + " " + thirdPart + " " + fourthPart + " " + " " + sixth + " " + checkedInput
+alert(allPart)
+}
